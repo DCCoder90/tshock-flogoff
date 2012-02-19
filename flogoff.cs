ef7b4083629c1e9a9adc52b82e4f5da237837577
Darkvengance aka Sildaekar <admin@rejectedfreaks.net>
2012-02-19 08:57:16 -0600

    Initial Commit

---
+++ flogoff.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TShockAPI;
+using Terraria;
+
+namespace Flogoff
+{
+    [APIVersion(1, 11)]
+
+    public class FLogoff : TerrariaPlugin
+    {
+        public override Version Version
+        {
+            get { return new Version("1.0.2"); }
+        }
+
+        public override string Name
+        {
+            get { return "FLogoff"; }
+        }
+
+        public override string Author
+        {
+            get { return "Darkvengance aka Sildaekar"; }
+        }
+
+        public override string Description
+        {
+            get { return "Performs a fake logoff"; }
+        }
+
+        public FLogoff(Main game)
+            : base(game)
+        {
+            Order = 4;
+        }
+
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command("kick", flogoff, "flogoff"));
+        }
+
+        private void flogoff(CommandArgs args)
+        {
+            TSPlayer player = args.Player;
+            Log.Info(string.Format("{0} did a fake logoff.", player.Name));
+            TSPlayer.All.SendMessage(string.Format("{0} left", player.Name), Color.Yellow);
+        }
+    }
+}
